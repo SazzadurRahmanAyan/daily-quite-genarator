6893332be06c2220b5a8aec8f023009b2e14eb90
Your NameSazzadurRahmanAyan <sazzadurrahman.ayan@outlook.com>
2023-05-31 22:52:32 +0600

    qoute api added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
 
 async function getqoute(url) {
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+    // console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
 }
 
 getqoute(api_url);
